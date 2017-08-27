498ef18dc821e89d88e968762cdf4b24ecefce4e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-08-27 19:52:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,52 @@
+function test() {
+  console.log("hello world");
+}
+
+function getFirstSelector(selector) {
+  console.log( document.querySelector(selector) )
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild2() {
+  const lis = document.querySelectorAll('div#nested div');
+  console.log(lis.length);
+  var nodes = [];
+  for (let i = 0; i < lis.length; i++) {
+    nodes.push(lis[i])
+  }
+  var curr = nodes.shift();
+  while (nodes.length > 1) {
+    if (curr.children.length > 0) {
+      for (let c = 0; c < curr.children.length; c++) {
+        nodes.push(curr.children[c])
+      }
+      curr = nodes.shift();
+    }
+  }
+  console.log(curr);
+  return curr
+
+}
+
+function deepestChild() {
+  const lis = document.querySelectorAll('div#nested div');
+  console.log(lis.length);
+  var node = lis[0];
+  while (node.children.length > 0) {
+    node = node.children[0]
+  }
+  console.log(node);
+  return node
+
+}
